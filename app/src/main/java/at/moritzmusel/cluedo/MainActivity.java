ddcccaa3d5bb25b89d9960a6470c8e3637f4c28c
Robert-Johann Dienst <rojo.dienst@gmail.com>
2022-04-07 13:45:46 +0200

    Die Logik funktioniert und ist als Würfel Klasse gebaut und nimmt als Wert ein ImageView an. Weiters habe ich eine Dice Animation eingebaut #2

--- app/src/main/java/at/moritzmusel/cluedo/MainActivity.java
+++ app/src/main/java/at/moritzmusel/cluedo/MainActivity.java
@@ -2,8 +2,11 @@ package at.moritzmusel.cluedo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, BoardActivity.class)));
         // Test push ch
         Log.i("Test", "Test");
     }
---
+++ app/src/main/java/at/moritzmusel/cluedo/game/Dice.java
@@ -0,0 +1,78 @@
+package at.moritzmusel.cluedo.game;
+
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+import at.moritzmusel.cluedo.R;
+
+public class Dice {
+    private int numberRolled;
+    private ImageView diceView;
+
+    /**
+     * Simuliert die Logik eines Würfels, sowie die Würfel Animation
+     * @param diceView die Image View des Würfels welcher der Spieler angezeigt
+     *                 bekommt bevor dieser ihn würfelt. Diese wird dann durch ein
+     *                 Bilde der gewürfelten zahl getauscht
+     */
+    public Dice(ImageView diceView){
+        this.diceView = diceView;
+    }
+
+    /**
+     * setzt den Würfel auf das ursprüngliche Image zurück
+     * setzt die gewürfelte Zahl auf -1
+     */
+    public void resetDice(){
+        diceView.setImageResource(R.drawable.dice3d);
+        numberRolled = -1;
+    }
+
+    /**
+     * Wirft den würfel. Dabei wird die Anmiation des Wurfes durchgeführt
+     * und das Ergebnis wird auf der Image View bildlich dargestellt
+     */
+
+    public void throwDice()  {
+        Random r = new Random();
+        int i = r.nextInt(4)+1;
+        Animation animation = AnimationUtils.loadAnimation(diceView.getContext(), R.anim.rotate_dice);
+        diceView.startAnimation(animation);
+        switch (i){
+            case 1:
+                diceView.setImageResource(R.drawable.dice1);
+                break;
+            case 2:
+                diceView.setImageResource(R.drawable.dice2);
+                break;
+            case 3:
+                diceView.setImageResource(R.drawable.dice3);
+                break;
+            case 4:
+                diceView.setImageResource(R.drawable.dice4);
+                break;
+        }
+        numberRolled = i;
+    }
+
+
+    //------------------------------------------ Getter and Setter --------------------------------------------------------------------------//
+    public ImageView getDiceView() {
+        return diceView;
+    }
+
+    public void setDiceView(ImageView diceView) {
+        this.diceView = diceView;
+    }
+
+    public int getNumberRolled() {
+        return numberRolled;
+    }
+
+    public void setNumberRolled(int numberRolled) {
+        this.numberRolled = numberRolled;
+    }
+}
