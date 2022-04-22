ad4f252da7bcc0d7b2952f8aaba1ee91ddbdf288
SMoserGH <s2moser@edu.aau.at>
2022-04-22 20:57:54 +0200

    Issue #11: CreateLobbyActivity.java updated

--- app/src/main/java/at/moritzmusel/cluedo/CreateLobbyActivity.java
+++ app/src/main/java/at/moritzmusel/cluedo/CreateLobbyActivity.java
@@ -5,9 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class CreateLobbyActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class CreateLobbyActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ListView playerlist;
+    private ArrayList<String> playerItems = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private int playerCounter = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,11 @@ public class CreateLobbyActivity extends AppCompatActivity implements View.OnCli
         send_link.setOnClickListener(this);
         final Button start = findViewById(R.id.btn_lobby_start);
         start.setOnClickListener(this);
+
+        playerlist = findViewById(R.id.playerlist);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, playerItems);
+        playerlist.setAdapter(adapter);
     }
 
     @Override
@@ -24,15 +38,23 @@ public class CreateLobbyActivity extends AppCompatActivity implements View.OnCli
         if(view.getId() == R.id.btn_send_link){
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");//Hier wird dann der Einladungslink weitergesendet.
             sendIntent.setType("text/plain");
 
             Intent shareIntent = Intent.createChooser(sendIntent, null);
             startActivity(shareIntent);
 
+            addPlayer(view);
+
         }
         if(view.getId() == R.id.btn_startgame){
             //select the character screen
+
         }
+    }
+
+    public void addPlayer(View view) {
+        playerItems.add("player "+playerCounter++);
+        adapter.notifyDataSetChanged();
     }
 }
