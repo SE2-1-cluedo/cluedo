f0f2ef4615e1d3efd052c351c470e9f66a189f67
SMoserGH <s2moser@edu.aau.at>
2022-04-23 20:10:54 +0200

    Issue #9: Weapon Cards updated

--- app/src/main/java/at/moritzmusel/cluedo/AllTheCards.java
+++ app/src/main/java/at/moritzmusel/cluedo/AllTheCards.java
@@ -15,5 +15,12 @@ public class AllTheCards {
     public Card colonelMustard = new Card(4, "Colonel Mustard", CardType.PERSON);
     public Card drOrchid = new Card(5, "Dr. Orchid", CardType.PERSON);
 
+    public Card dagger = new Card(6, "Dagger", CardType.WEAPON);
+    public Card candlestick = new Card(7, "Candlestick", CardType.WEAPON);
+    public Card pistol = new Card(8, "Pistol", CardType.WEAPON);
+    public Card rope = new Card(9, "Rope", CardType.WEAPON);
+    public  Card pipe = new Card(10, "Pipe", CardType.WEAPON);
+    public Card wrench = new Card(11, "Wrench", CardType.WEAPON);
+
 
 }
