e7c173a360c8e3cb0d718b5e8f8ce4b8e74f3c99
SMoserGH <s2moser@edu.aau.at>
2022-04-27 15:59:21 +0200

    Issue #11: CreateLobbyActivity.java updated

--- app/src/main/java/at/moritzmusel/cluedo/CreateLobbyActivity.java
+++ app/src/main/java/at/moritzmusel/cluedo/CreateLobbyActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -26,19 +27,22 @@ public class CreateLobbyActivity extends AppCompatActivity implements View.OnCli
         send_link.setOnClickListener(this);
         final Button start = findViewById(R.id.btn_lobby_start);
         start.setOnClickListener(this);
+        final Button back = findViewById(R.id.btn_back);
+        back.setOnClickListener(this);
+        TextView join_id = findViewById(R.id.txt_lobbyid);
+        join_id.setText(getGameID());
 
         playerlist = findViewById(R.id.playerlist);
-
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, playerItems);
         playerlist.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View view) {
-        if(view.getId() == R.id.btn_back){
+        if(view.getId() == R.id.btn_send_link){
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");//Hier wird dann der Einladungslink weitergesendet.
+            sendIntent.putExtra(Intent.EXTRA_TEXT, getGameID());//Hier wird dann der Einladungslink weitergesendet.
             sendIntent.setType("text/plain");
 
             Intent shareIntent = Intent.createChooser(sendIntent, null);
@@ -51,6 +55,16 @@ public class CreateLobbyActivity extends AppCompatActivity implements View.OnCli
             //select the character screen
 
         }
+        if(view.getId() == R.id.btn_back){
+            Intent i = new Intent(CreateLobbyActivity.this, LobbyDecisionActivity.class);
+            startActivity(i);
+        }
+    }
+
+    public String getGameID() {
+        //Schnittstelle mit dem Netzwerk um die id zu bekommen.
+        String id = "12345";//Nur zum sehen ob es geht
+        return id;
     }
 
     public void addPlayer(View view) {
