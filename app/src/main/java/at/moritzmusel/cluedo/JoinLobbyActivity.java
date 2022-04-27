8cd9e5c188c236587045d2810f931589e02ba7cf
SMoserGH <s2moser@edu.aau.at>
2022-04-27 16:39:07 +0200

    Issue #11: LobbyWaitingActivity.java und JoinLobbyActivity.java updated

--- app/src/main/java/at/moritzmusel/cluedo/JoinLobbyActivity.java
+++ app/src/main/java/at/moritzmusel/cluedo/JoinLobbyActivity.java
@@ -2,13 +2,49 @@ package at.moritzmusel.cluedo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class JoinLobbyActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class JoinLobbyActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText enter_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_lobby);
+        final Button back = findViewById(R.id.btn_back_to_lobbydecision);
+        back.setOnClickListener(this);
+        final Button join = findViewById(R.id.btn_lobby_join);
+        join.setOnClickListener(this);
+        enter_id = findViewById(R.id.txt_enter_id);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_back_to_lobbydecision){
+            Intent i = new Intent(JoinLobbyActivity.this, LobbyDecisionActivity.class);
+            startActivity(i);
+        }
+        if(view.getId() == R.id.btn_lobby_join){
+            if(enter_id.getText().equals(getGameId())){
+                Intent i = new Intent(JoinLobbyActivity.this, LobbyWaitingActivity.class);
+                startActivity(i);
+            }
+        }
+    }
+
+    public String getGameId() {
+        //Muss noch mit Netwerk verknüpt werden.
+        String id = "12345";
+        return id;
     }
 }
