962511d6417f73b037d7d754a45a9165dbab2319
TheTintin123 <kobenischke@edu.aau.at>
2022-04-28 17:58:01 +0200

    #26 First Board Prototype finished

--- app/src/main/java/at/moritzmusel/cluedo/BoardActivity.java
+++ app/src/main/java/at/moritzmusel/cluedo/BoardActivity.java
@@ -2,6 +2,7 @@ package at.moritzmusel.cluedo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -12,13 +13,55 @@ public class BoardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.board);
-
-        setContentView(R.layout.activity_board);
+        setContentView(R.layout.test_board2);
 
         ImageView diceView = findViewById(R.id.diceView);
         Dice dice = new Dice(diceView);
         diceView.setOnClickListener(view -> dice.throwDice());
 
+    }
+
+    public void onButtonClick(View v) {
+        String room = getResources().getResourceEntryName(v.getId());
+
+        //System.out.println(getResources().getResourceEntryName(v.getId())+" has been clicked");
+
+        switch(room){
+            case "lounge_button":
+                setContentView(R.layout.activity_room_lounge);
+                break;
+            case "conservatory_button":
+                setContentView(R.layout.activity_room_conservatory);
+                break;
+            case "ballroom_button":
+                setContentView(R.layout.activity_room_ballroom);
+                break;
+            case "dining_button":
+                setContentView(R.layout.activity_room_dining);
+                break;
+            case "kitchen_button":
+                setContentView(R.layout.activity_room_kitchen);
+                break;
+            case "library_button":
+                setContentView(R.layout.activity_room_library);
+                break;
+            case "billiard_button":
+                setContentView(R.layout.activity_room_billiard);
+                break;
+            case "study_button":
+                setContentView(R.layout.activity_room_study);
+                break;
+            case "hall_button":
+                setContentView(R.layout.activity_room_hall);
+                break;
+        }
+    }
+
+    public void onButtonReturn(View v){
+        setContentView(R.layout.test_board2);
+
+        ImageView diceView = findViewById(R.id.diceView);
+        Dice dice = new Dice(diceView);
+        diceView.setOnClickListener(view -> dice.throwDice());
     }
 }
