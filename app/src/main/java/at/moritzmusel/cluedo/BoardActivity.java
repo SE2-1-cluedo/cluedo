14842e70810796ad3e369d92f3b74d7984c173f6
TheTintin123 <kobenischke@edu.aau.at>
2022-05-08 22:00:55 +0200

    #33 Navigation and Status bar removed

--- app/src/main/java/at/moritzmusel/cluedo/BoardActivity.java
+++ app/src/main/java/at/moritzmusel/cluedo/BoardActivity.java
@@ -1,8 +1,6 @@
 package at.moritzmusel.cluedo;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActionBar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,15 +8,43 @@ import android.widget.ImageView;
 import at.moritzmusel.cluedo.game.Dice;
 
 public class BoardActivity extends AppCompatActivity {
+
+    private View decorView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        decorView = getWindow().getDecorView();
+        decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility){
+                if(visibility == 0)
+                    decorView.setSystemUiVisibility(hideSystemBars());
+            }
+        });
+
         setContentView(R.layout.test_board2);
 
         ImageView diceView = findViewById(R.id.diceView);
         Dice dice = new Dice(diceView);
         diceView.setOnClickListener(view -> dice.throwDice());
+    }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus)
+            decorView.setSystemUiVisibility(hideSystemBars());
+    }
+
+    private int hideSystemBars(){
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
     }
 
     public void onButtonClick(View v) {
