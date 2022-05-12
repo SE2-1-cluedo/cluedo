3794b4bf1e544a287bddcda850bbd676c10f44cb
SMoserGH <s2moser@edu.aau.at>
2022-05-12 09:45:14 +0200

    Issue #38: ShakeDetector updated

--- app/src/main/java/at/moritzmusel/cluedo/sensor/ShakeDetector.java
+++ app/src/main/java/at/moritzmusel/cluedo/sensor/ShakeDetector.java
@@ -18,19 +18,45 @@ public class ShakeDetector implements SensorEventListener {
         this.onShakeListener = listener;
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
-
     public interface OnShakeListener {
         void onShake(int count);
 
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+        if (onShakeListener != null) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            float gX = x / SensorManager.GRAVITY_EARTH;
+            float gY = y / SensorManager.GRAVITY_EARTH;
+            float gZ = z / SensorManager.GRAVITY_EARTH;
+            float gForce = (float)Math.sqrt(gX * gX + gY * gY + gZ * gZ);
+
+            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                final long now = System.currentTimeMillis();
+                if (shakeTimestamp + SHAKE_SLOP_TIME_MS > now) {//only detects shake event if it's a few seconds after a previous shake event
+
+                    return;
+                }
+
+                if (shakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {//shake count is reset after a few seconds
+                    shakeCount = 0;
+                }
+
+                shakeTimestamp = now;
+                shakeCount++;
+
+                onShakeListener.onShake(shakeCount);
+            }
+        }
     }
 
 
