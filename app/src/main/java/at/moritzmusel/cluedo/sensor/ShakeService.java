50df7a41a9b8a7b968c240e9dd4f68b934e5a868
SMoserGH <s2moser@edu.aau.at>
2022-05-12 09:48:42 +0200

    Issue #38: ShakeService updated

--- app/src/main/java/at/moritzmusel/cluedo/sensor/ShakeService.java
+++ app/src/main/java/at/moritzmusel/cluedo/sensor/ShakeService.java
@@ -1,4 +1,38 @@
 package at.moritzmusel.cluedo.sensor;
 
-public class ShakeService {
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.app.Service;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class ShakeService extends Service implements SensorEventListener {
+    private float accel; //acceleration ignoring gravity
+    private float accelGrav; //acceleration including gravity
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        float accelGravLast = accelGrav; //assigning current acceleration to latest acceleration
+        accelGrav = (float) Math.sqrt(x * x + y * y + z * z);
+        float delta = accelGrav - accelGravLast;
+        accel = accel * 0.9f + delta; // perform low-cut filter
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
