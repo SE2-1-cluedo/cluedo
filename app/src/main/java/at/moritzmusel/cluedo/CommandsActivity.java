38083d9af937f8612b0f517e94759c7cc74b7bf2
SMoserGH <s2moser@edu.aau.at>
2022-06-15 20:39:33 +0200

    Issue #23: Commands updated

--- app/src/main/java/at/moritzmusel/cluedo/CommandsActivity.java
+++ app/src/main/java/at/moritzmusel/cluedo/CommandsActivity.java
@@ -3,12 +3,25 @@ package at.moritzmusel.cluedo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class CommandsActivity extends AppCompatActivity {
+public class CommandsActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_commands);
+        back = findViewById(R.id.btn_back_commands);
+        back.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_back_commands){
+            finish();
+        }
     }
 }
