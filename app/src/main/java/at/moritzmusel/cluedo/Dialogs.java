ed1fde2752b92918b97669732d650c2b661920ec
SMoserGH <s2moser@edu.aau.at>
2022-06-25 18:31:40 +0200

    Issue #68: Dialog updated

--- app/src/main/java/at/moritzmusel/cluedo/Dialogs.java
+++ app/src/main/java/at/moritzmusel/cluedo/Dialogs.java
@@ -6,9 +6,18 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import at.moritzmusel.cluedo.entities.Character;
+import at.moritzmusel.cluedo.entities.Player;
 
 public class Dialogs {
     Dialog dialog;
@@ -82,6 +91,51 @@ public class Dialogs {
         Intent intent = new Intent(ac.getApplicationContext(),MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         ac.startActivity(intent);
+    }
+
+    public void callCheatDialog(Activity ac, List<Player> players){
+        final String[] framed = {""};
+
+        dialog = new Dialog(ac);
+        dialog.setContentView(R.layout.cheat_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        ImageView img_close = dialog.findViewById(R.id.img_close);
+        Button btn_frame = dialog.findViewById(R.id.btn_frame);
+
+        String[] player_characters = null;
+        int i = 0;
+        for(Player p:players){
+            player_characters[i] = p.getPlayerCharacterName().name();
+            i++;
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ac,android.R.layout.simple_list_item_1,player_characters);
+
+        ListView framable_players = (ListView)dialog.findViewById(R.id.list_framable_players);
+
+        framable_players.setAdapter(adapter);
+
+        framable_players.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //framed[0] = player_characters[position];
+                Toast.makeText(ac, "You framed " + player_characters[position], Toast.LENGTH_LONG).show();
+            }
+        });
+
+        TextView txt_frame = dialog.findViewById(R.id.txt_cheat_title);
+        //txt_frame.setText("Frame: " + framed[0]);
+
+        img_close.setOnClickListener(v -> {
+            dialog.dismiss();
+        });
+
+        btn_frame.setOnClickListener(v -> {
+            Toast.makeText(ac, "You framed " + framed[0], Toast.LENGTH_LONG).show();
+            dialog.dismiss();
+        });
+        dialog.show();
     }
 
 
